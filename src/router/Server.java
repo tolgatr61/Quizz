e548d2a702dc82ebcda53bc96ddc3f2d296dca99
tolgatr61 <47320521+tolgatr61@users.noreply.github.com>
2020-07-07 23:19:50 +0200

    Improvements of Router. (Client / Server connectivity)

--- src/router/Server.java
+++ src/router/Server.java
@@ -2,6 +2,8 @@ package src.router;
 
 import java.net.*;
 import java.io.*;
+
+import javax.sound.sampled.Port;
 import javax.xml.parsers.ParserConfigurationException;
 import org.xml.sax.SAXException;
 
@@ -10,11 +12,15 @@ public class Server {
     public static void main(String[] args) throws ParserConfigurationException,
     SAXException, IOException {
         ServerSocket ss = new ServerSocket(4999);
+        System.out.println("Server succesfuly online with Port : 4999.\nWaiting for client connections...");
         Boolean run = true;
-        while (run = true){
-        Socket s = ss.accept();
 
-        System.out.println("Client connected");
+        Integer clientNumber = 0;
+
+        while (run = true){
+            Socket s = ss.accept();
+            clientNumber += 1;
+            System.out.println("Client : " + String.valueOf(clientNumber) + " succesfuly connected.");
         }
     }
 }
